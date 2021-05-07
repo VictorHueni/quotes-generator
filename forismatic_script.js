9ef13aa73056692f138e539c65874951484e8b3c
Victor Hüni <victor.hueni@protonmail.com>
2021-05-07 17:29:31 +0200

    add forismatic_script.js file & create heroku cors proxxy server

---
+++ forismatic_script.js
@@ -0,0 +1,17 @@
+//Get quote from api
+async function getQuote() {
+    const proxyUrl = 'https://salty-lake-93108.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        getQuote();
+        console.log('whoops, no quote.', error);
+    }
+}
+
+
+// On load
+getQuote();
